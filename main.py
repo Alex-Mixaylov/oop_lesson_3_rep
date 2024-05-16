1280358d9f738941044a913dc22a48dc49b3e52a
Alex <guardamar81@gmail.com>
2024-05-16 22:05:49 +0400

    update

--- main.py
+++ main.py
@@ -1,35 +1,76 @@
 # Принципы SOLID
 # Принцип единственной ответственности
 
-class UserManager():
-    def __init__(self, user):
-        self.user = user
+# class UserManager():
+#     def __init__(self, user):
+#         self.user = user
+#
+#     def change_user_name(self, new_name):
+#         self.user.name = new_name
+#
+#     def save_user(self):
+#         file = open("users.txt", "a")
+#         file.write(self.user)
+#         file.close()
+#
+#
+# class User():
+#     def __init__(self, name):
+#         self.name = name
+#
+# class UserNameChanger():
+#     def __init__(self, user):
+#         self.user = user
+#
+#     def change_user_name(self, new_name):
+#         self.user.name = new_name
+#
+# class SaveUser():
+#     def __init__(self, user):
+#         self.user = user
+#
+#     def save_user(self):
+#         file = open("users.txt", "a")
+#         file.write(self.user)
+#         file.close()
 
-    def change_user_name(self, new_name):
-        self.user.name = new_name
+# Принцип закрытости открытости ОСР
 
-    def save_user(self):
-        file = open("users.txt", "a")
-        file.write(self.user)
-        file.close()
+# class Report():
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
+#
+#     def docPrinter(self):
+#         print(f'Сформирован отчет {self.title} с содержанием {self.content}')
 
-
-class User():
-    def __init__(self, name):
-        self.name = name
-
-class UserNameChanger():
-    def __init__(self, user):
-        self.user = user
-
-    def change_user_name(self, new_name):
-        self.user.name = new_name
-
-class SaveUser():
-    def __init__(self, user):
-        self.user = user
-
-    def save_user(self):
-        file = open("users.txt", "a")
-        file.write(self.user)
-        file.close()
+#
+# from abc import ABC, abstractmethod
+#
+# class Formated(ABC):
+#     @abstractmethod
+#     def format(self, report):
+#         pass
+#
+# class TextFormated(Formated):
+#     def format(self, report):
+#         print(report.title)
+#         print(report.content)
+#
+# class HTMLFormated(Formated):
+#     def format(self, report):
+#         print(f"<h1>{report.title}</h1>")
+#         print(f"<p>{report.content}</p>")
+#
+# class Report():
+#     def __init__(self, title, content, formated):
+#         self.title = title
+#         self.content = content
+#         self.formated = formated
+#
+#     def docPrinter(self):
+#         self.formated.format(self)
+#
+# report = Report('заголовок отчета', 'это текст отчета его тут много', HTMLFormated())
+#
+# report.docPrinter()
