a6100e838280fd5c9997cebb77deabdcf74e123c
Alex <guardamar81@gmail.com>
2024-05-17 20:26:17 +0400

    update main.py

--- main.py
+++ main.py
@@ -174,3 +174,41 @@ reader_audiobook = StoryReader(audiobook)
 
 reader_book.tell_story()
 reader_audiobook.tell_story()
+
+# решение ДЗ
+
+from abc import ABC, abstractmethod
+
+class Weapon(ABC):
+    @abstractmethod
+    def attack(self):
+        pass
+
+class Sword(Weapon):
+    def attack(self):
+        print('Боец наносит удар мечом')
+
+class Bow(Weapon):
+    def attack(self):
+        print('Боец стреляет из лука')
+
+class Fighter():
+    def __init__(self, weapon: Weapon): # указываем какой тип должен хранится в пременной- объект класса Weapon
+        self.weapon = weapon
+    def change_weapon(self, weapon: Weapon):
+        self.weapon = weapon
+
+    def fight(self):
+        print(self.weapon.attack())
+
+class Monster():
+    pass
+
+sword1 = Sword()
+bow1 = Bow()
+
+fighter1 = Fighter(sword1)
+fighter1.fight()
+fighter1.change_weapon(bow1)
+fighter1.fight()
+    
